8a726adcc31c87adab208aaebd61f2136c6f0bd0
Hugo Gonzalez <hugo.gonzales@mirpartners.com>
2016-11-12 16:27:19 -0800

    Initial Commit

---
+++ CoreService/CoreService.h
@@ -0,0 +1,19 @@
+//
+//  CoreService.h
+//  CoreService
+//
+//  Created by hugogonzalez on 11/12/16.
+//  Copyright © 2016 Cat Bakery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreService.
+FOUNDATION_EXPORT double CoreServiceVersionNumber;
+
+//! Project version string for CoreService.
+FOUNDATION_EXPORT const unsigned char CoreServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreService/PublicHeader.h>
+
+
