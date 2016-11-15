d8fb8c5b78bf420889657d08b77ca1d3c02c6244
Hugo Gonzalez <hugo.gonzales@mirpartners.com>
2016-11-14 16:35:37 -0800

    added alamo fire source and set singleton for CSNetwork

---
+++ CoreService/CSNetwork.swift
@@ -0,0 +1,13 @@
+//
+//  CSNetwork.swift
+//  CoreService
+//
+//  Created by hugogonzalez on 11/14/16.
+//  Copyright © 2016 Cat Bakery. All rights reserved.
+//
+
+import UIKit
+
+open class CSNetwork: NSObject {
+    open static let main = CSNetwork()
+}
